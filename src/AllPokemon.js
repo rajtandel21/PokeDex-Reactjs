11016590fa397f39a36e2260867c958bf1b4d41e
Raj Tandel <rajtandel@hotmail.com>
2020-07-23 14:52:53 +0100

    Initial commit

---
+++ src/AllPokemon.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+//requirements and notes
+//make multiple api calls to show list of pokemon.
+//clicking on the pokemon will display data of that pokemon on top.
+
+//https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png (pokemon image, change ID at end).
+
+class AllPokemon extends react.Component {
+  render() {
+    return <div>Loading...</div>;
+  }
+}
+
+export default AllPokemon;
