bd5653024c7af90247471fbdf00251e1d6080829
Raj Tandel <rajtandel@hotmail.com>
2020-08-17 19:53:47 +0100

    style update

--- src/BigPokemonCard.js
+++ src/BigPokemonCard.js
@@ -23,6 +23,11 @@ function BigPokemonCard({
 }) {
   const [defaultImage, setDefaultImage] = useState();
 
+  const bigStyle = {
+    width: (attack / 255) * 100,
+    backgroundColor: "#4caf50",
+  };
+
   useEffect(() => {
     if (id > 807) {
       if (altImage !== null) {
@@ -44,23 +49,41 @@ function BigPokemonCard({
     <div className="bigCard" onClick={closeCard}>
       <div className="details">
         <div className="smallDetails">
-          <h1>{`#${id} ${name}`}</h1>
-
           <img src={defaultImage} alt="Pokemon not found"></img>
+          <h1>{`#${id} ${name}`}</h1>
           <p>Type: {type}</p>
         </div>
+
         <div className="stats">
+          <p>Base Stats</p>
+          <p>Hp: {hp}</p>
+          <div className="statsBar">
+            <div className="statsPercentage hp"></div>
+          </div>
+          <p>Attack: {attack}</p>
+          <div className="statsBar">
+            <div className="statsPercentage attack" style={bigStyle}></div>
+          </div>
+          <p>Special Attack: {specialAttack}</p>
+          <div className="statsBar">
+            <div className="statsPercentage spAttack"></div>
+          </div>
+          <p>Defense: {defense}</p>
+          <div className="statsBar">
+            <div className="statsPercentage defense"></div>
+          </div>
+          <p>Special Defense: {specialDefense}</p>
+          <div className="statsBar">
+            <div className="statsPercentage spDefense"></div>
+          </div>
+          <p>Speed: {speed}</p>
+          <div className="statsBar">
+            <div className="statsPercentage speed"></div>
+          </div>
           <p>Abilities: {abilities}</p>
           <p>
             Height: {height * 10}cm Weight: {weight / 10}kg
           </p>
-          <p>Bast Stats:</p>
-          <p>Hp: {hp}</p>
-          <p>Attack: {attack}</p>
-          <p>Special Attack: {specialAttack}</p>
-          <p>Defense: {defense}</p>
-          <p>Special Defense: {specialDefense}</p>
-          <p>Speed: {speed}</p>
         </div>
       </div>
       <span>&#10060;</span>
